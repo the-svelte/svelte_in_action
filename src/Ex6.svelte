6c505434e834fd680abec8b61111430f67c0fa96
YoungPhilGim <youngphil.gim@kasset.co.kr>
2022-02-21 14:04:36 +0900

    fixed

--- src/Ex6.svelte
+++ src/Ex6.svelte
@@ -4,6 +4,7 @@ import Ex611 from './ch06/Ex6.1.1.svelte';
 import Ex620 from './ch06/Ex6.2.0.svelte';
 import Ex621 from './ch06/Ex6.2.1.svelte';
 import App from './ch06/App.svelte';
+import Ex650 from './ch06/Ex6.5.0.svelte';
 </script>
 
 <!-- <h3>6.1</h3>
@@ -11,6 +12,9 @@ import App from './ch06/App.svelte';
 <Ex611/>
 <h3>6.2</h3>
 <Ex620/>
-<Ex621/> -->
+<Ex621/>
 <h3>6.4</h3>
 <App/>
+-->
+<h3>6.5</h3>
+<Ex650/>
---
+++ src/ch06/Ex6.5.0.svelte
@@ -0,0 +1,24 @@
+<script>
+  import {dogStore} from './dogStore';
+  import {bigDogsStore} from './bigDogsStore';
+
+  $dogStore = {
+    'aaa': { id: 'aaa', name: 'A0', breed: 'a0', size: 'small' },
+    'bbb': { id: 'bbb', name: 'B1', breed: 'b1', size: 'large' },
+    'ccc': { id: 'ccc', name: 'C2', breed: 'c2', size: 'medium' },
+    'ddd': { id: 'ddd', name: 'D3', breed: 'd3', size: 'large' },
+  };
+</script>
+
+<div>
+  <h4>Dogs</h4>
+  {#each Object.values($dogStore) as dog}
+    <p>{dog.name} : {dog.breed} : {dog.size}</p>
+  {/each}
+</div>
+<div>
+  <h4>Big Dogs</h4>
+  {#each $bigDogsStore as dog}
+    <p>{dog.name} {dog.size}</p>
+  {/each}
+</div>
