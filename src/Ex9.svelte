45c8587c0fec076a76c4d8db4e5f6a721d70e2be
YoungPhil <youngphil.gim@gmail.com>
2022-02-27 12:29:48 +0900

    fixed

--- src/Ex9.svelte
+++ src/Ex9.svelte
@@ -1,9 +1,12 @@
 <script>
   import Ex910 from './ch09/Ex9.1.0.svelte';
-  // import Ex920 from './ch09/Ex9.2.0.svelte';
+  import Ex920 from './ch09/Ex9.2.0.svelte';
   // import Ex930 from './ch09/Ex9.3.0.svelte';
   // import Ex940 from './ch09/Ex9.4.0.svelte';
 </script>
 
-<h3>9.1</h3>
-<Ex910/>
+<!-- <h3>9.1</h3>
+<Ex910/> -->
+
+<h3>9.2</h3>
+<Ex920/>
---
+++ src/ch09/Ex9.2.0.svelte
@@ -0,0 +1,29 @@
+<script>
+  import NotFound from './NotFound.svelte';
+  import Shop from './Shop.svelte';
+  import Cart from './Cart.svelte';
+  import Ship from './Ship.svelte';
+
+  let component;
+
+  const hashMap = {
+    '#shop': Shop,
+    '#cart': Cart,
+    '#ship': Ship,
+  };
+
+  function hashChange() {
+    component = hashMap[location.hash] || NotFound;
+  }
+</script>
+
+<svelte:window on:hashchange={hashChange} />
+<nav class="nav nav-tabs">
+  <a href="/#shop" class="nav-link" class:active={component === Shop}>Shop</a>
+  <a href="/#cart" class="nav-link" class:active={component === Cart}>Cart</a>
+  <a href="/#ship" class="nav-link" class:active={component === Ship}>Ship</a>
+</nav>
+
+<main>
+  <svelte:component this={component} />
+</main>
